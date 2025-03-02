f860daea759162633b5b5aa51d8997d647c1a67d
Gabriel <gabrieloliveira280807@gmail.com>
2025-03-02 18:23:44 -0300

    Iterando no JSX, Adicionando os posts em vetor

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,40 @@ import { Sidebar } from './components/Sidebar';
 import { Header } from './components/Header';
 import { Post } from './components/Post';
 
+const posts = [
+  {
+    id:1,
+    author: {
+        avatarUrl: 'https://avatars.githubusercontent.com/u/129549638?v=4',
+        name: 'Ricardo Gabriel',
+        role: 'Estudante de programação'
+      },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-03-01 10:15:05'),
+  },
+  {
+    id:2,
+    author:
+      {
+        avatarUrl: 'https://xesque.rocketseat.dev/users/avatar/profile-129549638-1728329132987.jpg',
+        name: 'Ricardo',
+        role: 'Estudante de programação'
+      },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-03-01 10:15:05'),
+  },
+]
+
+
+
 function App() {
   return (
     <>
@@ -14,8 +48,16 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                 author={post.author}
+                 content={post.content}
+                 publishedAt={post.publishedAt}
+              />
+
+            );
+          })}
         </main>
       </div>
     </>
