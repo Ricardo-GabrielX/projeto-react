6f128f13ddde3bd21b6c84b086e54f3612b6dd9b
Gabriel <gabrieloliveira280807@gmail.com>
2025-03-03 17:04:38 -0300

    Fazendo funcionar a funçao de deletar comentário.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -36,8 +36,12 @@ export function Post({ author, publishedAt, content }){
         setNewCommentText(event.target.value);
     };
 
-    function deleteComment(comment){
-        console.log(`Deletar comentário ${comment}`)
+    function deleteComment(commentToDelete){
+        // Imutabilidade -> as variaveis não sofrem mutação, nos criamos um novo valor (um novo espaço na memoria).
+        const commentWithoutDeleteOne = comments.filter(comment => {
+            return comment != commentToDelete;
+        })
+        setComments(commentWithoutDeleteOne);
     };
 
     return(
