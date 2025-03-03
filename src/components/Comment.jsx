a53325cf9f81d6e1f8f544a40d3ee6fd6a6fcc5c
Gabriel <gabrieloliveira280807@gmail.com>
2025-03-03 16:54:25 -0300

    Entendendo o conceito de comunicação entre componentes.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,7 +4,10 @@ import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css"
 
 
-export function Comment({ content }){
+export function Comment({ content , onDeleteComment}){
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    };
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={ false } src="https://avatars.githubusercontent.com/u/129549638?v=4" />
@@ -20,7 +23,7 @@ export function Comment({ content }){
                             </time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
                             <Trash size={24} />
                         </button>
                     </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -36,6 +36,10 @@ export function Post({ author, publishedAt, content }){
         setNewCommentText(event.target.value);
     };
 
+    function deleteComment(comment){
+        console.log(`Deletar comentário ${comment}`)
+    };
+
     return(
         <article className={styles.post}>
             <header>
@@ -81,7 +85,13 @@ export function Post({ author, publishedAt, content }){
             </form>
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment} /> 
+                    return (
+                    <Comment 
+                        key={comment} 
+                        content={comment} 
+                        onDeleteComment={deleteComment}
+                    /> 
+                )
                 }) }
                 
             </div>
