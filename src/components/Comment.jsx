95a7baf88f41314f9b1555a8aad810b703642c39
Gabriel <gabrieloliveira280807@gmail.com>
2025-03-04 16:17:51 -0300

    Adicionando validação no formulário e realizando likes nos comentários.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,12 +2,18 @@ import { ThumbsUp, Trash } from  "@phosphor-icons/react";
 import { Avatar } from "./Avatar";
 
 import styles from "./Comment.module.css"
-
+import { useState } from "react";
 
 export function Comment({ content , onDeleteComment}){
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment(){
         onDeleteComment(content);
     };
+    
+    function handleLikeComment(){
+        setLikeCount(likeCount + 1);
+    }
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={ false } src="https://avatars.githubusercontent.com/u/129549638?v=4" />
@@ -31,9 +37,9 @@ export function Comment({ content , onDeleteComment}){
 
                 </div>
                 <footer>
-                    <button> 
+                    <button onClick={handleLikeComment}> 
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,8 +33,13 @@ export function Post({ author, publishedAt, content }){
     };
 
     function handleNewCommentChange(){
+        event.target.setCustomValidity("");
         setNewCommentText(event.target.value);
     };
+
+    function handleNewCommentInvalid(){
+        event.target.setCustomValidity("Esse campo é obrigatório!")
+    }
 
     function deleteComment(commentToDelete){
         // Imutabilidade -> as variaveis não sofrem mutação, nos criamos um novo valor (um novo espaço na memoria).
@@ -43,6 +48,9 @@ export function Post({ author, publishedAt, content }){
         })
         setComments(commentWithoutDeleteOne);
     };
+    
+    const isNewCommentEmpty = newCommentText.length === 0;
+
 
     return(
         <article className={styles.post}>
@@ -65,10 +73,10 @@ export function Post({ author, publishedAt, content }){
 
             <div className={styles.content}>
                {content.map(line => {
-                if(line.type === 'paragraph'){
+                if(line.type === "paragraph"){
                     return <p key={line.content}>{line.content}</p>;
                 }
-                else if(line.type === 'link'){
+                else if(line.type === "link"){
                     return <p key={line.content}><a href="#">{line.content}</a></p>;
                 }
                })}
@@ -78,13 +86,17 @@ export function Post({ author, publishedAt, content }){
                 <strong>Deixe seu feedback</strong>
                 <textarea
                     name="comment"
-                    placeholder='Deixe um comentario'
+                    placeholder="Deixe um comentario"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
             <div className={styles.commentList}>
